1bd1865bae443e0c5c07d8d4058d8659fe0fe288
Ishant Solanki <ishant.solanki@ef.com>
2025-11-03 21:40:12 +0000

    Move files to components and assets folders

--- src/Applications.tsx
+++ src/Applications.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import SingleApplication from "./SingleApplication";
 import styles from "./Applications.module.css";
 import { useApplications } from "./network/applications";
+import { Loading } from "./components/Loading/Loading";
 
 const Applications = () => {
-  const { data } = useApplications(1, 10);
+  const { data, isLoading } = useApplications(1, 10);
   return (
     <div className={styles.Applications}>
-      {data?.map((application) => (
-        <SingleApplication key={application.guid} application={application} />
-      ))}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        data?.map((application) => (
+          <SingleApplication key={application.guid} application={application} />
+        ))
+      )}
     </div>
   );
 };
