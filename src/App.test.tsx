5a7136e0083000d8dd6f6d8750a71fab418516e9
Ishant Solanki <ishant.solanki@ef.com>
2025-11-03 22:45:33 +0000

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
 test('renders "Application Portal" title', () => {
   render(<App />);
   const linkElement = screen.getByText(/Application portal/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders "Load more" button', () => {
+  render(<App />);
+  const loadMoreButton = screen.getByRole("button", { name: /loading\.\.\./i });
+  expect(loadMoreButton).toBeInTheDocument();
 });
